60d075b3cf3521f0bf9ef66b2c71c85d38ee5216
Lucas Matos <lucaslmatos18@gmail.com>
2023-09-22 16:10:22 -0300

    REQ 03 - Class PessoaEleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,33 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  /**
+     * Construtor da classe PessoaCandidata.
+     *
+     * @param novoNome O nome da pessoa eletiroa.
+     * @param novoCpf  O código de pessoa fisica da pessoa eleitora.
+     */
+
+  public PessoaEleitora(String novoNome, String novoCpf) {
+    this.setNome(novoNome);
+    this.setCpf(novoCpf);
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String novoCpf) {
+    this.cpf = novoCpf;
+  }
 }
